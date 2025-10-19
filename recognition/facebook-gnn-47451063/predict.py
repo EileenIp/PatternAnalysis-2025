70a695ea519fcef665dbfeebd4da0637b5d7f837
EileenIp <eileenip01@gmail.com>
2025-10-19 20:56:13 +1000

    Edited the umap parameters of run function in predict.py

--- recognition/facebook-gnn-47451063/predict.py
+++ recognition/facebook-gnn-47451063/predict.py
@@ -1,8 +1,6 @@
-# predict.py
 from train import run_model
 from pathlib import Path
 
-# Configuration
 project_root = Path(__file__).resolve().parents[1]
 data_dir = project_root / "data"
 edges_csv = data_dir / "musae_facebook_edges.csv"
@@ -10,9 +8,19 @@ target_csv = data_dir / "musae_facebook_target.csv"
 feats_json = data_dir / "musae_facebook_features.json"
 
 SEED = 42
-SVD_COMPONENTS = 256   # reduce BoW features to this dim for speed+memory
+SVD_COMPONENTS = 256
+
+MAX_UMAP = 8000
+UMAP_N_NEIGHBORS = 15
+UMAP_MIN_DIST = 0.05
+UMAP_METRIC = "cosine"
+
 
 run_model(edges_csv, target_csv, feats_json, 
           svd_components=SVD_COMPONENTS, 
           seed=SEED, 
+          MAX_UMAP=MAX_UMAP,
+          UMAP_N_NEIGHBORS=UMAP_N_NEIGHBORS,
+          UMAP_MIN_DIST=UMAP_MIN_DIST,
+          UMAP_METRIC=UMAP_METRIC
           )
