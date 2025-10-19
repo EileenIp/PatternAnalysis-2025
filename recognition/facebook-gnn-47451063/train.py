75d46401f91087aab94c8cf05a1fd502589bd494
EileenIp <eileenip01@gmail.com>
2025-10-19 21:40:35 +1000

    Added GAT training curve to train.py

--- recognition/facebook-gnn-47451063/train.py
+++ recognition/facebook-gnn-47451063/train.py
@@ -203,3 +203,5 @@ def run_model(edges_path,
     show_tsne("GCN", gcn_model, data, max_points=MAX_TSNE, perplexity=TSNE_PERPLEXITY, n_iter=TSNE_ITER, seed=seed)
     show_umap("GCN", gcn_model, data, max_points=MAX_UMAP, UMAP_N_NEIGHBORS=UMAP_N_NEIGHBORS, UMAP_MIN_DIST=UMAP_MIN_DIST, UMAP_METRIC=UMAP_METRIC, seed=seed)
     plot_training_curves("GCN", gcn_hist)
+    
+    plot_training_curves("GAT", gat_hist)
