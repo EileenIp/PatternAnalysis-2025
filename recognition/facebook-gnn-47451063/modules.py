7a13e85a094e77008dd1e9953042390026f60779
EileenIp <eileenip01@gmail.com>
2025-10-19 21:30:58 +1000

    Added GAT to modeules.py

--- recognition/facebook-gnn-47451063/modules.py
+++ recognition/facebook-gnn-47451063/modules.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, GATConv
 
 class GCNModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, dropout=0.6):
@@ -18,5 +18,19 @@ class GCNModel(nn.Module):
     
     def embed(self, data):
         x, edge_index = data.x, data.edge_index
-        embedding = F.relu(self.conv1(x, edge_index))
-        return embedding
+        return F.relu(self.conv1(x, edge_index))
+
+class GATModelBasic(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout=0.6, heads=8):
+        super().__init__()
+        self.conv1 = GATConv(input_dim, hidden_dim, heads=heads, dropout=dropout, concat=True)
+        self.conv2 = GATConv(hidden_dim*heads, output_dim, heads=1, dropout=dropout, concat=False)
+        self.dropout = dropout
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        hidden = F.dropout(x, p=self.dropout, training=self.training)
+        hidden = F.elu(self.conv1(hidden, edge_index))
+        hidden = F.dropout(hidden, p=self.dropout, training=self.training)
+        out = self.conv2(hidden, edge_index)
+        return out
